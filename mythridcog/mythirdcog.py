0095863634006da56d2a7424ed63f407db254d2e
Redjumpman <Redjumpman@users.noreply.github.com>
2018-08-10 23:53:45 -0500

    Update mythirdcog.py

--- mythridcog/mythirdcog.py
+++ mythridcog/mythirdcog.py
@@ -1,19 +1,12 @@
-from discord.ext import commands
+from redbot.core import commands
 
 
 # Classname should be CamelCase and the same spelling as the folder
 class MyThirdCog:
     """Description of the cog visible with [p]help MyThirdCog"""
 
-    def __init__(self, bot):
-        self.bot = bot
-
     @commands.command()
-    async def mythirdcom(self):
+    async def mythirdcom(self, ctx):
         """Description of myfirstcom visible with [p]help mythirdcom"""
         # Your code will go here
-        pass
-
-
-def setup(bot):
-    bot.add_cog(MyThirdCog(bot))
+        await ctx.send("My third cog!")
