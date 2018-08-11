d7d24f9bfd23254ac8501e3b381eb633185a05c0
Redjumpman <Redjumpman@users.noreply.github.com>
2018-08-10 23:51:24 -0500

    Update mysecondcog.py

--- mysecondcog/mysecondcog.py
+++ mysecondcog/mysecondcog.py
@@ -1,19 +1,12 @@
-from discord.ext import commands
+from redbot.core import commands
 
 
 # Classname should be CamelCase and the same spelling as the folder
 class MySecondCog:
     """Description of the cog visible with [p]help MySecondCog"""
 
-    def __init__(self, bot):
-        self.bot = bot
-
     @commands.command()
-    async def mysecondcom(self):
+    async def mysecondcom(self, ctx):
         """Description of myfirstcom visible with [p]help mysecondcom"""
         # Your code will go here
-        pass
-
-
-def setup(bot):
-    bot.add_cog(MySecondCog(bot))
+        await ctx.send("My second cog!")
